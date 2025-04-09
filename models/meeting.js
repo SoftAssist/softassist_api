0df4b7207dc9543daaaa77f78bc64e7fa57157d3
Jonathan Meffert <jlmeffert@yahoo.com>
2025-04-08 21:15:34 -0400

    SCRUM-11 backend provide recorded meetings to whisper api to transcribe MP4 is not supported, and conversion using cloudconvert isn't working yet... it might just be timeout.

--- models/meeting.js
+++ models/meeting.js
@@ -7,7 +7,12 @@ const meetingSchema = new mongoose.Schema({
     audioFile: { 
         data: Buffer,
         contentType: String
-    }
+    },
+    audioFileId: { type: mongoose.Schema.Types.ObjectId },
+    transcript: { type: String, required: false }
 });
+
+// Add compound index to ensure meetingName is unique within a project
+meetingSchema.index({ projectId: 1, meetingName: 1 }, { unique: true });
 
 module.exports = mongoose.model('Meeting', meetingSchema); 
